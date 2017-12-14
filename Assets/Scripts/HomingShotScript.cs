fd779e9cfc99b1fa0c3ebf077fb177c07af1dd7a
上田知洋 <uedatomohiro@uedatomohironoMacBook-Pro.local>
2017-12-14 16:30:43 +0900

    homingのスピードを遅く

--- Assets/Scripts/HomingShotScript.cs
+++ Assets/Scripts/HomingShotScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HomingShotScript : MonoBehaviour {
-	float speed = 15;
+	float speed = 10;
 	GameObject target;
 	public GameObject homingSound;
 	public GameObject effect;
